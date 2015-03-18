523003b8544314e202fd1d229f4d99644c46bc3e
Abhinav <abhinavdhere@gmail.com>
2015-03-18 22:33:26 +0530

    Initial Commit

---
+++ calc.cpp
@@ -0,0 +1,26 @@
+//A calculator with a MATLAB like interface i.e. enter the command and get answer
+// Author: Abhinav Dhere ; Date: 18/03/15
+
+#include <iostream>
+
+using namespace std;
+
+int number(string n){
+int num;
+switch (n){
+case 1:
+    num=1;
+    return num;
+case 2:
+    num=2
+}
+cout<<n;
+return 0;
+}
+
+int main(void){
+string a;
+a="Hello";
+number(a);
+return 0;
+}
